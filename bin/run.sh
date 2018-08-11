cc3e4bc7b930b06bd2438900de8c92a60bd6710a
kometchtech <5137077+kometchtech@users.noreply.github.com>
2018-08-11 23:40:26 +0900

    fix digitalocean

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 syslog-ng
-goofys -f --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE $BUCKET $MOUNT_DIR
+goofys -f --endpoint $ENDPOINT --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE --profile ${PROFILE} $BUCKET $MOUNT_DIR
 
